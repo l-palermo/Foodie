359518bc29da2a2db81e87d9f58aa2ed97429143
Danielandro <mtme13@hotmail.com>
2019-09-24 15:54:57 +0100

    UI: create dumb component for form input
    Co-authored-by: lucianmot <lucian.i.mot@gmail.com>

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Input from '../Components/Input'
 
 class Signup extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      newUser: {
+        name: ""
+      }
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   handleSubmit(e) {
@@ -11,17 +19,38 @@ class Signup extends Component {
     e.preventDefault();
   }
 
+  // handleFullName(e) {
+  //   let value = e.target.value;
+  //   this.setState(prevState => ({
+  //     newUser:
+  //       { ...prevState.newUser, name: value }
+  //   }))
+  // }
+
+  handleInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+
+    this.setState(prevState => {
+      return {
+        newUser:
+          { ...prevState.newUser, [name]: value }
+      }
+
+    }, () => console.log(this.state.newUser)
+    )
+
+  }
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" ref={(input) => this.input = input} />
-        </label>
-        <span>Hello World</span>
-
-        <input type="submit" value="Sign Up" />
-      </form >
+      <Input type={'text'}
+        title={'Full Name'}
+        name={'name'}
+        value={this.state.newUser.name}
+        placeholder={'Enter Your Name'}
+        handleChange={this.handleInput}
+      />
     );
   }
 
