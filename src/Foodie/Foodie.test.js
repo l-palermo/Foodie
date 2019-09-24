6bb48bbeecd66a444fad11c251eea6cc9720f681
Danielandro <mtme13@hotmail.com>
2019-09-24 12:43:04 +0100

    UI: create signup form

---
+++ src/Foodie/Foodie.test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Signup from '../Signup/Signup'
+
+class Homepage extends Component {
+
+
+}
---
+++ src/Signup/Signup.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    alert(`The value is ${this.input.value}`);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" ref={(input) => this.input = input} />
+        </label>
+        <span>Hello World</span>
+
+        <input type="submit" value="Sign Up" />
+      </form >
+    );
+  }
+
+}
+
