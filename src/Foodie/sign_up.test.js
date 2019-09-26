f48c23fb24b810e28b594327881f716d0e9282b1
lucianmot <lucian.i.mot@gmail.com>
2019-09-26 13:34:54 +0100

    added last changes

---
+++ src/Foodie/sign_up.test.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('input[name="name"]');
+    await page.type('input[name="name"]', 'pokemon');
+    await page.click('Button');
+    expect(1).toEqual(1);
+    await browser.close();
+})();
